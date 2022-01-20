65791521ae8d548801a7fdc9259707e833834ca8
parth-30 <74844729+parth-30@users.noreply.github.com>
2022-01-19 18:47:19 -0600

    Add files via upload

---
+++ class.py
@@ -0,0 +1,17 @@
+class Car(object):
+    def __init__(self,model,company,topspeed,color):
+        self.model=model
+        self.company=company
+        self.topspeed=topspeed
+        self.color=color 
+
+    def display(self):
+        print("Hi")
+        return("Hello")
+
+    def show(self):
+        print("how are you")
+
+c1 = Car("velar","land rover","250km/h", "black")
+print(c1.topspeed)
+print(c1.display())
